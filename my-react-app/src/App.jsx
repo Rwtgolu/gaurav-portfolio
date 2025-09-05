7805d91f3959d6c2a7ad5e49dfad4feef383a04a
Gaurav singh <golurawat151106@gmail.com>
2025-09-05 20:33:28 +0530

    adding all the file

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import { Nav, Main, Project, Education, Skills, Contact ,Footer} from './card.jsx';
+
+function App() {
+  return (
+    <> 
+      <Nav />
+      <Main />
+      <Project />
+      <Education />
+      <Skills />
+      <Contact />
+      <Footer/>
+
+
+    </> 
+  );
+}
+
+export default App;
